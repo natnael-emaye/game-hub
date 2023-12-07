ef4c76bee382ef4838f9a7e6db5b5b5eba1d909f
natnael-emaye <natnaelabebegualu@gmail.com>
2023-12-07 11:54:49 +0300

    build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <div className="">
+            <Grid
+                templateAreas={{
+                    base: `"nav" "main"`,
+                    lg: `"nav nav"
+                          "aside main"`,
+                }}
+            >
+                <GridItem pl="2" bg="green" h="50" area={"nav"}>
+                    Header
+                </GridItem>
+                <Show above="lg">
+                    <GridItem pl="2" bg="red" area={"aside"}>
+                        aside
+                    </GridItem>
+                </Show>
+                <GridItem pl="2" h="100%" bg="black" area={"main"}>
+                    Main
+                </GridItem>
+            </Grid>
+        </div>
+    );
 }
 
-export default App
+export default App;
