661c35e531938cc8d692f57ac8e0bb85b60f95b5
natnael-emaye <natnaelabebegualu@gmail.com>
2023-12-13 13:03:25 +0300

    Build card component

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
+import PlatformIconsList from "./PlatformIconList";
 
 interface Props {
     game: Game;
@@ -10,7 +11,8 @@ export default function GameCard({ game }: Props) {
         <Card rounded={10} overflow="hidden">
             <Image src={game.background_image} />
             <CardBody>
-                <Heading size="xl" >{game.name}</Heading>
+                <Heading size="xl">{game.name}</Heading>
+                <PlatformIconsList platforms={game.parent_platforms.map(({platforms})=>platforms)} />
             </CardBody>
         </Card>
     );
--- src/hooks/useGames.tsx
+++ src/hooks/useGames.tsx
@@ -2,9 +2,17 @@ import { useEffect, useState } from "react";
 import { AxiosError, CanceledError } from "axios";
 import apiClient from "../utils/api-client";
 
-interface Game {
+export interface PlatformProps {
     id: number;
     name: string;
+    slug: string;
+}
+
+export interface Game {
+    id: number;
+    name: string;
+    background_image: string;
+    parent_platforms: { platforms: PlatformProps }[];
 }
 
 interface FetchGamesResponse {
@@ -15,20 +23,28 @@ interface FetchGamesResponse {
 export default function useGames() {
     const [games, setGames] = useState<Game[]>([]);
     const [error, setError] = useState("");
+    const [isLoading, setLoading] = useState(false);
+
     const controller = new AbortController();
-    
+
     useEffect(() => {
+        setLoading(true);
         apiClient
             .get<FetchGamesResponse>("/games", {
                 signal: controller.signal,
             })
-            .then((res) => setGames(res.data.results))
+            .then((res) => {
+                setGames(res.data.results);
+                setLoading(true);
+            })
+
             .catch((error: AxiosError) => {
                 if (error instanceof CanceledError) return;
                 setError(error.message);
+                setLoading(true);
             });
-        return () => controller.abort();
+        // return () => controller.abort();
     }, []);
 
-    return { games, error };
+    return { games, error,isLoading };
 }
