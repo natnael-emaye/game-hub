3f9b699ddf0f986f709e7b47832b5bb3f18a3196
natnael-emaye <natnaelabebegualu@gmail.com>
2023-12-16 21:05:02 +0300

    Displaying genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
+import getOptimizedImage from "../utils/image-url";
 
 function GenreList() {
-    const { data:genres, error, isLoading } = useGenre();
+    const { data: genres, error, isLoading } = useGenre();
     return (
-        <ul>
+        <List>
             {genres.map((genre) => (
-                <li key={genre.id}>{genre.name}</li>
+                <ListItem paddingY={1} key={genre.id}>
+                    <HStack padding={1}>
+                        <Image
+                            borderRadius={6}
+                            boxSize="32px"
+                            src={getOptimizedImage(genre.image_background)}
+                        />
+                        <Text fontSize="lg">{genre.name}</Text>
+                    </HStack>
+                </ListItem>
             ))}
-        </ul>
+        </List>
     );
 }
 
