6d6a0fe64b3162551b2e102f2faeac5263630def
natnael-emaye <natnaelabebegualu@gmail.com>
2023-12-17 20:54:15 +0300

    showing the spinner while fetching the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,9 +1,12 @@
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import useGenre from "../hooks/useGenre";
 import getOptimizedImage from "../utils/image-url";
-
+// lesson 97 genres skeleton
 function GenreList() {
     const { data: genres, error, isLoading } = useGenre();
+    if (error) return null;
+    if (isLoading) return <Spinner />;
+
     return (
         <List>
             {genres.map((genre) => (
