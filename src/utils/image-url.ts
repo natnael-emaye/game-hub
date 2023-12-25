b60bcdf252b4a0900d84a8a9af2bbd5235c433ca
natnael-emaye <natnaelabebegualu@gmail.com>
2023-12-25 10:04:09 +0300

    handling games with no image

--- src/utils/image-url.ts
+++ src/utils/image-url.ts
@@ -1,4 +1,7 @@
+import noImage from "../assets/no-image.webp"
+
 export default function getOptimizedImage(image: string) {
+    if(!image) return noImage
     const url = image.split("media/");
     const firsturl = url[0] + "media/";
     return firsturl + "crop/600/400/" + url[1];
