14aed8bd877caaa0a1e7342ed8793716fd2c84c7
natnael-emaye <natnaelabebegualu@gmail.com>
2023-12-25 11:18:44 +0300

    refactor: render conditionally

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,7 +9,7 @@ interface Prop {
     gameQuery: GameQueryProps;
 }
 
-export default function GameGrid({ gameQuery}: Prop) {
+export default function GameGrid({ gameQuery }: Prop) {
     const { data: games, error, isLoading } = useGames(gameQuery);
     const skeleton = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
@@ -17,7 +17,11 @@ export default function GameGrid({ gameQuery}: Prop) {
         <>
             {error && <Heading>{error}</Heading>}
             <Heading marginY={2} size="3xl" fontWeight="700">
-                {!error ? (gameQuery.genre ? gameQuery.genre.name : "New and trading") : ""}
+                {!error
+                    ? gameQuery.genre
+                        ? gameQuery.genre.name
+                        : "New and trading"
+                    : ""}
             </Heading>
             <SimpleGrid
                 columns={{
@@ -29,17 +33,17 @@ export default function GameGrid({ gameQuery}: Prop) {
                 padding="10px"
                 spacing={7}
             >
-                {isLoading &&
-                    skeleton.map((index) => (
-                        <GameCardContainer key={index}>
-                            <GameCardSkeleton />
-                        </GameCardContainer>
-                    ))}
-                {games.map((game) => (
-                    <GameCardContainer key={game.id}>
-                        <GameCard game={game} />
-                    </GameCardContainer>
-                ))}
+                {isLoading
+                    ? skeleton.map((index) => (
+                          <GameCardContainer key={index}>
+                              <GameCardSkeleton />
+                          </GameCardContainer>
+                      ))
+                    : games.map((game) => (
+                          <GameCardContainer key={game.id}>
+                              <GameCard game={game} />
+                          </GameCardContainer>
+                      ))}
             </SimpleGrid>
         </>
     );
