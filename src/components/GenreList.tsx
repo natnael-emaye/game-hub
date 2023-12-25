bf1528a27abc9ef86e829f9503c4656548c7e7a8
natnael-emaye <natnaelabebegualu@gmail.com>
2023-12-25 13:30:57 +0300

    refactor: clean up the genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
     Button,
     HStack,
+    Heading,
     Image,
     List,
     ListItem,
@@ -20,30 +21,36 @@ function GenreList({ selectedGenre, onSelectedGenre }: Props) {
     if (isLoading) return <Spinner />;
 
     return (
-        <List>
-            {genres.map((genre) => (
-                <ListItem paddingY={1} key={genre.id}>
-                    <HStack padding={1}>
-                        <Image
-                            borderRadius={6}
-                            boxSize="32px"
-                            src={getOptimizedImage(genre.image_background)}
-                        />
-                        <Button
-                            onClick={() => onSelectedGenre(genre)}
-                            variant="link"
-                            fontWeight={
-                                selectedGenre?.id == genre.id
-                                    ? "bold"
-                                    : "normal"
-                            }
-                        >
-                            {genre.name}
-                        </Button>
-                    </HStack>
-                </ListItem>
-            ))}
-        </List>
+        <>
+            <Heading size="lg" marginBottom={2}>Genres</Heading>
+            <List>
+                {genres.map((genre) => (
+                    <ListItem paddingY={1} key={genre.id}>
+                        <HStack padding={1}>
+                            <Image
+                                objectFit="cover"
+                                borderRadius={6}
+                                boxSize="32px"
+                                src={getOptimizedImage(genre.image_background)}
+                            />
+                            <Button
+                                whiteSpace="pre-wrap"
+                                textAlign="left"
+                                onClick={() => onSelectedGenre(genre)}
+                                variant="link"
+                                fontWeight={
+                                    selectedGenre?.id == genre.id
+                                        ? "bold"
+                                        : "normal"
+                                }
+                            >
+                                {genre.name}
+                            </Button>
+                        </HStack>
+                    </ListItem>
+                ))}
+            </List>
+        </>
     );
 }
 
